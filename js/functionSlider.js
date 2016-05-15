846b48380c8a45313580a2bdaaac6294e012544c
Bogdan Alin <alinugr@gmail.com>
2016-05-16 01:39:17 +0200

    nuevo menu ,localizacion nueva ,slider nuevo

--- js/functionSlider.js
+++ js/functionSlider.js
@@ -1,25 +1,40 @@
-//Slide automatico
-var slideIndex = 0;
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("slideAuto");
+var slideIndex = 5;
+showDivs(slideIndex);
 
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    slideIndex++;
-
-    if (slideIndex > x.length) {
-        slideIndex = 1
-    }
-    x[slideIndex - 1].style.display = "block";
-    setTimeout(carousel, 5000);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
 
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideAuto");
+  var dots = document.getElementsByClassName("demo");
+  
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+      slideIndex = x.length;
+     
+    } 
+    
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+ 
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
 
 var slideBarIndex = 0;
-
 function funSlideBar() {
     var i;
     var x = document.getElementsByClassName("slideBar");
@@ -33,63 +48,11 @@ function funSlideBar() {
         slideBarIndex = 1
     }
     x[slideBarIndex - 1].style.display = "block";
-    setTimeout(funSlideBar, 8000);
+    setTimeout(funSlideBar, 2000);
 }
 
 
-//Slide manual
-var slideIndexA = 1;
-var slideIndexB = 1;
 
-function plusDivs(n, nameDiv) {
-    if (nameDiv == "slideA") {
-        showDivs(slideIndexA += n, nameDiv);
-    }
-    if (nameDiv == "slideB") {
-        showDivs(slideIndexB += n, nameDiv);
-    }
-}
-
-function currentDiv(n, nameDiv) {
-    if (nameDiv == "slideA") {
-        showDivs(slideIndexA = n, nameDiv);
-    }
-    if (nameDiv == "slideB") {
-        showDivs(slideIndexB = n, nameDiv);
-    }
-}
-
-function showDivs(n, nameDiv) {
-    //console.log("function: showDiv param: "+n+" "+nameDiv);
-    var i;
-    var x = document.getElementsByClassName(nameDiv);
-    //console.log(x);
-
-    if (n > x.length && nameDiv == "slideA") {
-        slideIndexA = 1
-    }
-    if (n > x.length && nameDiv == "slideB") {
-        slideIndexB = 1
-    }
-
-    if (n < 1 && nameDiv == "slideA") {
-        slideIndexA = x.length
-    }
-    if (n < 1 && nameDiv == "slideB") {
-        slideIndexB = x.length
-    }
-
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-
-    if (nameDiv == "slideA") {
-        x[slideIndexA - 1].style.display = "block";
-    }
-    if (nameDiv == "slideB") {
-        x[slideIndexB - 1].style.display = "block";
-    }
-}
 
 function changeContextMenu(option) {
     var x = document.getElementsByClassName("menu");
@@ -97,5 +60,5 @@ function changeContextMenu(option) {
         x[i].className = "menu w3-padding-medium";
     }
     
-    document.getElementById("menu"+option).className = "menu w3-padding-medium w3-black";
+    document.getElementById("menu"+option).className = "menu w3-padding-medium w3-theme-d1";
 }
